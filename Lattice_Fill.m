5ddd9fede41c05fe76d471513b7061689804c612
NivLakshman <nlakshmanan3@gatech.edu>
2015-02-24 12:32:14 -0500

    Generates lattice points

--- Lattice_Fill.m
+++ Lattice_Fill.m
@@ -9,6 +9,7 @@ function [MonList, Edges] = Lattice_Fill(MS,pw,ph,lsx,lsy)
 % lsx = lattice spacing x
 % lsy = lattice spacing y
 
+MonList = [];
 [h, w, bins] = size(MS);
 h = h*ph;
 w = w*pw;
@@ -48,10 +49,11 @@ for i = 1:bins
     Y_bottomleft = [Img_Corners_Rot(Bottom_Corner,2) Img_Corners_Rot(Left_Corner,2)];
     line_bottomleft = polyfit(X_bottomleft, Y_bottomleft,1);
     
-    xbox = [Img_Corners_Rot(:,1)' Img_Corners_Rot(1,1)];
-    ybox = [Img_Corners_Rot(:,2)' Img_Corners_Rot(1,2)];
+%     xbox = [Img_Corners_Rot(:,1)' Img_Corners_Rot(1,1)];
+%     ybox = [Img_Corners_Rot(:,2)' Img_Corners_Rot(1,2)];
     
-    Intersect = [Img_Corners_Rot(Bottom_Corner,:); Img_Corners_Rot(Top_Corner,:)];
+    Intersect = [Img_Corners_Rot(Bottom_Corner,:)];
+    crystalxy = Intersect./[lsx lsy];
     
     
     for y = min_y+lsy:lsy:max_y-lsy
@@ -65,6 +67,7 @@ for i = 1:bins
         end
         
         new_intersect1 = [line_1(1:2); line_2(1:2)]\[line_1(3);line_2(3)];
+        crystal_edge1 = new_intersect1'./[lsx lsy];
         
         % Left intersection
         if y <= Img_Corners_Rot(Left_Corner,2)
@@ -74,10 +77,26 @@ for i = 1:bins
         end
         
         new_intersect2 = [line_1(1:2); line_2(1:2)]\[line_1(3);line_2(3)];
+        crystal_edge2 = new_intersect2'./[lsx lsy];
+        
         Intersect = [Intersect; new_intersect1'; new_intersect2'];
         
-   end
-    
+        for x = ceil(crystal_edge2(1)):floor(crystal_edge1(1))
+            crystalxy = [crystalxy; [x y]];  
+        end
+        
+    end
+    xy_Rot = ones(size(crystalxy));
+    for j = 1:length(xy_Rot)
+        xy_Rot(j,:) = crystalxy(j,:).*[lsx lsy];
+    end
+    Lattice_list = Straighten_List(xy_Rot, Bin_Angle);
+    for j = 1:length(Lattice_list)
+        if IsInFiber(Lattice_list(j,:),MSi,pw,ph)
+            MonList = [MonList; Lattice_list(j,:)];
+        end
+    end
+    Edges = Straighten_List(Intersect, Bin_Angle);
     
 end
 
@@ -91,4 +110,34 @@ Rotated = [];
    for i = 1:size(PtList,1)
        Rotated(i,:) = ROT*PtList(i,:)';
    end
+end
+
+function Straightened = Straighten_List(Rot_List,angle)
+
+Straight = [cos(angle) sin(angle);
+            sin(angle) -cos(angle)];
+Straightened = [];
+
+for i = 1:size(Rot_List)
+    Straightened(i,:) = Straight*Rot_List(i,:)';
+end
+
+end
+
+function out = IsInFiber(RSpt,MS,pw,ph)
+%% IsInFiber
+% Checks if a point in fiber space is actually within the pixels of a fiber
+% in real space
+
+pixel_space = fliplr(RSpt./[pw ph]);   % 1st dimension of pixel space is the negative y-axis of real space...
+pixel_ind = ceil(pixel_space);
+[m,n] = size(MS);
+
+if pixel_ind(1)<1 || pixel_ind(2)<1 || pixel_ind(1)>m || pixel_ind(2)>n
+    out = 0;
+elseif MS(pixel_ind(1), pixel_ind(2)) == -180
+    out = 0;
+else
+    out = 1;
+end
 end
