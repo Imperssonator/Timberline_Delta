6420462f4e9d2af488aa9b70fefd4877fb8f27f2
NivLakshman <nlakshmanan3@gatech.edu>
2015-02-26 11:28:38 -0500

    Commented lattice fill

--- Lattice_Fill.m
+++ Lattice_Fill.m
@@ -24,25 +24,24 @@ for i = 1:1 % Right now just working on bin 1
     Img_Corners = [0 0;...
         w 0;...
         0 h;...
-        w h];
+        w h]; % Edges of the frame marked
     MSi = MS(:,:,i);
-    Bin_Angle = MSi(find(MSi,1))/180*pi;
+    Bin_Angle = MSi(find(MSi,1))/180*pi; % Finds the first non-zero value in the bin
     if isempty(Bin_Angle)
         break
     end
-    Img_Corners_Rot = Rotate_List(Img_Corners,Bin_Angle);
+    Img_Corners_Rot = Rotate_List(Img_Corners,Bin_Angle); % Rotate the frame by bin angle
     
     [max_y ,Top_Corner] = max(Img_Corners_Rot(:,2));
     [min_y ,Bottom_Corner] = min(Img_Corners_Rot(:,2));
     [max_x ,Right_Corner] = max(Img_Corners_Rot(:,1));
     [min_x ,Left_Corner] = min(Img_Corners_Rot(:,1));
-    Left_Side = min_x-1;        % Left side of a line segment that will completely span the diamond
-    Right_Side = max_x+1;
     
     % Equations for the lines of the box
     X_bottomright = [Img_Corners_Rot(Bottom_Corner,1) Img_Corners_Rot(Right_Corner,1)];
     Y_bottomright = [Img_Corners_Rot(Bottom_Corner,2) Img_Corners_Rot(Right_Corner,2)];
-    line_bottomright = polyfit(X_bottomright, Y_bottomright,1);
+    line_bottomright = polyfit(X_bottomright, Y_bottomright,1); % Fit the two vertices of the frame to give the edge
+    % y = ax + b, line_bottomright = [a b]
     
     X_topright = [Img_Corners_Rot(Top_Corner,1) Img_Corners_Rot(Right_Corner,1)];
     Y_topright = [Img_Corners_Rot(Top_Corner,2) Img_Corners_Rot(Right_Corner,2)];
@@ -55,31 +54,29 @@ for i = 1:1 % Right now just working on bin 1
     X_bottomleft = [Img_Corners_Rot(Bottom_Corner,1) Img_Corners_Rot(Left_Corner,1)];
     Y_bottomleft = [Img_Corners_Rot(Bottom_Corner,2) Img_Corners_Rot(Left_Corner,2)];
     line_bottomleft = polyfit(X_bottomleft, Y_bottomleft,1);
-    
-    %     xbox = [Img_Corners_Rot(:,1)' Img_Corners_Rot(1,1)];
-    %     ybox = [Img_Corners_Rot(:,2)' Img_Corners_Rot(1,2)];
-    
+   
     Intersect = [Img_Corners_Rot(Bottom_Corner,:)];
-    crystalxy = zeros(ceil((h/lsy)*(w/lsx)),2);
+    % gives the list of points on the edges of the frame
+    crystalxy = zeros(ceil((h/lsy)*(w/lsx)),2); % gives the list of points that fills the lattice in crystal space
 %     whos crystalxy
     crystalxy(1,:) = Intersect./[lsx lsy];
     county = 1;
     disp(min_y)
     disp(max_y)
     
-    for y = min_y+lsy:lsy:max_y-lsy
+    for y = min_y+lsy:lsy:max_y
         %         disp(y)
-        line_1 = [0 1 y]; % ax + by = c
+        line_1 = [0 1 y]; % ax + by = c for the y line
         
         % Right intersection
         if y<=Img_Corners_Rot(Right_Corner,2)
-            line_2 = [line_bottomright(1) -1 -1*line_bottomright(2)];
+            line_2 = [line_bottomright(1) -1 -1*line_bottomright(2)]; % line_2 = [a b c] for the edge of the frame
         else
             line_2 = [line_topright(1) -1 -1*line_topright(2)];
         end
         
-        new_intersect1 = [line_1(1:2); line_2(1:2)]\[line_1(3);line_2(3)];
-        crystal_edge1 = new_intersect1'./[lsx lsy];
+        new_intersect1 = [line_1(1:2); line_2(1:2)]\[line_1(3);line_2(3)]; % POI of line1 and line 2
+        crystal_edge1 = new_intersect1'./[lsx lsy]; 
         
         % Left intersection
         if y <= Img_Corners_Rot(Left_Corner,2)
@@ -95,19 +92,19 @@ for i = 1:1 % Right now just working on bin 1
         
         for x = ceil(crystal_edge2(1)):floor(crystal_edge1(1))
             county = county+1;
-            crystalxy(county,:) = [x y];
+            crystalxy(county,:) = [x y]; % Filling the box
         end
         
         
     end
     
     crystalxy = crystalxy(1:county,:); % remove tail-end zeros
-    xy_Rot = ones(size(crystalxy));
+    xy_Rot = ones(size(crystalxy)); % Lattice points in rotated space (nm)
     disp('Filled in box')
     for j = 1:length(xy_Rot)
-        xy_Rot(j,:) = crystalxy(j,:).*[lsx lsy];
+        xy_Rot(j,:) = crystalxy(j,:).*[lsx lsy]; % Scaling up to nm
     end
-    Lattice_list = Straighten_List(xy_Rot, Bin_Angle);
+    Lattice_list = Straighten_List(xy_Rot, Bin_Angle); % Brings back frame to real space
     disp('Rotated box back')
     MonList = zeros(size(Lattice_list));
     countm = 0;
@@ -116,7 +113,7 @@ for i = 1:1 % Right now just working on bin 1
         %         tic
         if IsInFiber(Lattice_list(j,:),MSi,pw,ph)
             countm = countm+1;
-            MonList(countm,:) = Lattice_list(j,:);
+            MonList(countm,:) = Lattice_list(j,:); % Checks if each lattice point has a monomer
         end
         %         checker = toc
     end
